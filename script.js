0df7f331c9b07f8656ae9319ddee0a9daa4e2a6a
topaz <haruki.30.30.11@gmail.com>
2023-05-16 20:47:35 +0900

    母音+数字で入力できるようにした。

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 var words = ["apple", "banana", "cherry", "dragonfruit", "elderberry"];
 var kanjiData;
 
+var sisei = [
+  ["a", "ā", "á", "ǎ", "à"],
+  ["e", "ē", "é", "ě", "è"],
+  ["o", "ō", "ó", "ǒ", "ò"],
+  ["i", "ī", "í", "ǐ", "ì"],
+  ["u", "ū", "ú", "ǔ", "ù"],
+  ["v", "ǖ", "ǘ", "ǚ", "ǜ"],
+];
+
 var wordElement = document.getElementById("word");
 var inputElement = document.getElementById("input");
 var startButton = document.getElementById("start-btn");
@@ -13,10 +22,23 @@ var currentWordIndex;
 
 startButton.addEventListener("click", startGame);
 
+function Init() {
+  // button
+  var elms = document.getElementsByClassName("AlphabetButton");
+  console.log(elms.length);
+  for (let index = 0; index < elms.length; index++) {
+    const element = elms[index];
+    // element.bu
+    element.addEventListener("click", () => {
+      console.log(inputElement.value);
+      inputElement.value = inputElement.value + element.textContent;
+      console.log(element.textContent);
+    });
+  }
+}
+
 function startGame() {
   currentWordIndex = 0;
-  // score = 0;
-  // scoreElement.textContent = "スコア: " + score;
 
   inputElement.value = "";
   inputElement.focus();
@@ -35,13 +57,44 @@ function generateWord() {
 inputElement.addEventListener("input", checkInput);
 
 function checkInput() {
-  if (inputElement.value.trim() === kanjiData[currentWordIndex][1]) {
-    // score++;
-    // scoreElement.textContent = "スコア: " + score;
-
+  // if (inputElement.value.trim() === kanjiData[currentWordIndex][1]) {
+  if (inputElement.value.trim() === "www") {
     inputElement.value = "";
     currentWordIndex++;
     generateWord();
+  }
+  var inputText = inputElement.value.trim();
+  var lastIndex = inputText.length;
+  console.log(inputText);
+  console.log(lastIndex);
+  if (inputText[lastIndex - 1] == "1") {
+    AddSisei(1);
+  }
+  if (inputText[lastIndex - 1] == "2") {
+    AddSisei(2);
+  }
+  if (inputText[lastIndex - 1] == "3") {
+    AddSisei(3);
+  }
+  if (inputText[lastIndex - 1] == "4") {
+    AddSisei(4);
+  }
+}
+
+function AddSisei(siseiValue) {
+  var inputText = inputElement.value.trim();
+  var lastIndex = inputText.length;
+  if (inputText.length >= 2) {
+    var changedCharacter = inputText[lastIndex - 2];
+    console.log(changedCharacter);
+    for (let index = 0; index < sisei.length; index++) {
+      const target = sisei[index][0];
+      if (target == changedCharacter) {
+        inputElement.value =
+          inputText.substring(0, lastIndex - 2) + sisei[index][siseiValue];
+        break;
+      }
+    }
   }
 }
 
@@ -81,3 +134,4 @@ function convertCSVtoArray(str) {
 }
 
 getCSV(); //最初に実行される
+Init();
