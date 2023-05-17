455a74173d8d9bc116aaf045155b96bfd8bce2da
topaz <haruki.30.30.11@gmail.com>
2023-05-17 11:26:04 +0900

    add space between button

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// 単語のリスト
-var words = ["apple", "banana", "cherry", "dragonfruit", "elderberry"];
+// ["漢字","拼音","意味"]
 var kanjiData;
 
-var sisei = [
+const sisei = [
   ["a", "ā", "á", "ǎ", "à"],
   ["e", "ē", "é", "ě", "è"],
   ["o", "ō", "ó", "ǒ", "ò"],
@@ -15,12 +14,10 @@ var wordElement = document.getElementById("word");
 var inputElement = document.getElementById("input");
 var startButton = document.getElementById("start-btn");
 
-var state = "READY";
-
 var currentWordIndex;
 
 function Init() {
-  // button
+  // Button Events
   var elms = document.getElementsByClassName("AlphabetButton");
   console.log(elms.length);
   for (let index = 0; index < elms.length; index++) {
@@ -41,27 +38,45 @@ function Init() {
       console.log(element.textContent);
     });
   }
-}
-
-function startGame() {
   currentWordIndex = 0;
-
   inputElement.value = "";
-  inputElement.focus();
 
   generateWord();
 }
 
-// key入力
+// KEY PRESSED
 document.addEventListener("keypress", keypress_ivent);
+document.addEventListener("keydown", keypress_ivent);
 function keypress_ivent(e) {
-  //いずれかのキーが押された時の処理
-  console.log(e.textContent);
-  console.log(e.code);
   console.log(e);
   if (97 <= e.keyCode && e.keyCode <= 122) {
     inputElement.value = inputElement.value + e.key;
   }
+  if (e.key == "1") {
+    AddSisei(1);
+  }
+  if (e.key == "2") {
+    AddSisei(2);
+  }
+  if (e.key == "3") {
+    AddSisei(3);
+  }
+  if (e.key == "4") {
+    AddSisei(4);
+  }
+
+  if (e.code == "Backspace") {
+    console.log("WWWWW");
+    if (inputElement.value.length > 0) {
+      console.log("WWWWW");
+      inputElement.value = inputElement.value.substring(
+        0,
+        inputElement.value.length - 1
+      );
+    }
+  }
+
+  // TODO: BACKSPACE
   return false;
 }
 
@@ -76,81 +91,50 @@ function generateWord() {
 inputElement.addEventListener("input", checkInput);
 
 function checkInput() {
-  // if (inputElement.value.trim() === kanjiData[currentWordIndex][1]) {
   if (inputElement.value.trim() === "www") {
     inputElement.value = "";
     currentWordIndex++;
     generateWord();
-  }
-  var inputText = inputElement.value.trim();
-  var lastIndex = inputText.length;
-  console.log(inputText);
-  console.log(lastIndex);
-  if (inputText[lastIndex - 1] == "1") {
-    AddSisei(1);
-  }
-  if (inputText[lastIndex - 1] == "2") {
-    AddSisei(2);
-  }
-  if (inputText[lastIndex - 1] == "3") {
-    AddSisei(3);
-  }
-  if (inputText[lastIndex - 1] == "4") {
-    AddSisei(4);
   }
 }
 
 function AddSisei(siseiValue) {
   var inputText = inputElement.value.trim();
   var lastIndex = inputText.length;
-  if (inputText.length >= 2) {
-    var changedCharacter = inputText[lastIndex - 2];
-    console.log(changedCharacter);
+  if (inputText.length >= 1) {
+    var changedCharacter = inputText[lastIndex - 1];
     for (let index = 0; index < sisei.length; index++) {
       const target = sisei[index][0];
       if (target == changedCharacter) {
         inputElement.value =
-          inputText.substring(0, lastIndex - 2) + sisei[index][siseiValue];
+          inputText.substring(0, lastIndex - 1) + sisei[index][siseiValue];
         break;
       }
     }
   }
 }
 
-function endGame() {
-  wordElement.textContent = "ゲーム終了";
-  inputElement.disabled = true;
-  // startButton.disabled = false;
-}
-
 //CSVファイルを読み込む関数getCSV()の定義
 function getCSV() {
-  console.log("hogeho");
-  var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
-  req.open("get", "./sample.csv", true); // アクセスするファイルを指定
-  req.send(null); // HTTPリクエストの発行
-
-  console.log("hogeho");
-
-  // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ
+  var req = new XMLHttpRequest();
+  req.open("get", "./pinyin.csv", true);
+  req.send(null);
   req.onload = function () {
-    convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    convertCSVtoArray(req.responseText);
   };
 }
-
-// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
 function convertCSVtoArray(str) {
-  // 読み込んだCSVデータが文字列として渡される
-  var result = []; // 最終的な二次元配列を入れるための配列
-  var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+  console.log(str);
+  var result = [];
+  var tmp = str.split("\n");
 
   // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
   for (var i = 0; i < tmp.length; ++i) {
     result[i] = tmp[i].split(",");
   }
-
   kanjiData = result;
+
+  Init();
 }
 
 getCSV(); //最初に実行される
-Init();
